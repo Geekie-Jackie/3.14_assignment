b305c0be286b8cd9cfe995f156c403b719c6af55
root <root@Starlight>
2023-09-10 23:46:21 +0800

    initial commit

--- src/main/java/com/shopping_cart/shopping_cart/controller/CatalogueController.java
+++ src/main/java/com/shopping_cart/shopping_cart/controller/CatalogueController.java
@@ -1,3 +1,4 @@
+// Assignment 3.14
 package com.shopping_cart.shopping_cart.controller;
 
 import java.util.List;
@@ -27,15 +28,21 @@ public class CatalogueController {
     @Autowired
     CatalogueRepository repo; // Added
 
+    // Scenario 3: Database connection is lost
     // Updated (produces = MediaType.APPLICATION_JSON_VALUE)
     @RequestMapping(value = "/catalogues", method = RequestMethod.GET, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<List<Catalogue>> list(@RequestParam(defaultValue = "") String search) {
-        List<Catalogue> results = repo.findByNameContaining(search);
-        System.out.println("Results Size: " + results.size());
-        if (results.size() == 0) {
-            return ResponseEntity.notFound().build();
-        } else {
-            return ResponseEntity.ok(results);
+        try {
+            List<Catalogue> results = repo.findByNameContaining(search);
+            System.out.println("Results Size: " + results.size());
+            if (results.size() == 0) {
+                return ResponseEntity.notFound().build();
+            } else {
+                return ResponseEntity.ok(results);
+            }
+        } catch (Exception e) {
+            // Scenario 3: Database connection is lost
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 
@@ -48,11 +55,12 @@ public class CatalogueController {
      * @RequestParam => /catalogues?id=1
      */
     @GetMapping(value = "/catalogues/{id}")
-    public ResponseEntity<Optional<Catalogue>> get(@PathVariable int id) {
+    public ResponseEntity<?> getCatalogueById(@PathVariable int id) {
         Optional<Catalogue> result = (Optional<Catalogue>) repo.findById(id);
         if (result.isPresent())
-            return ResponseEntity.ok(result);
-
+            // Scenario 2: Catalogue Id is present
+            return ResponseEntity.ok(result.get());
+        // Scenario 1: Catalogue Id is not found
         return ResponseEntity.notFound().build();
     }
 
@@ -70,7 +78,8 @@ public class CatalogueController {
             return ResponseEntity.created(null).body(created);
         } catch (Exception e) {
             System.out.println(e);
-            return ResponseEntity.internalServerError().build();
+            // Scenario 3: Database connection is lost
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
 
     }
